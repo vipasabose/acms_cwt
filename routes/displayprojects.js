8be041fc4dc25d61ed2e566c4d446ddab0d5a37d
vipasabose <vipasa.bose@gmail.com>
2019-04-29 19:35:31 +0530

    updated displayproject

--- routes/displayprojects.js
+++ routes/displayprojects.js
@@ -11,12 +11,71 @@ function projectdesc(req,res,next){
   }=body;
 
   Version.find({projectname:pid,versionNum:versionNum},(err,project)=>{
+console.log(project.length);
     if(err){
       res.status(404).send({
         success: false,
         message:'Error: Server error'});
     }
-    else{
+    else if(project.length===0)
+    {
+      console.log('I was here');
+      Project.find({_id:pid},(err,p)=>{
+        if(err){
+          res.status(404).send({
+            success: false,
+            message:'Error: Server error'});
+        }
+        else {
+          var projectreviewer;
+          var contributors = new Array();
+          var contributornames = new Array();
+          contributors = p[0].contributors;
+          Reviewer.find({_id:p[0].reviewer},(err,prreviewer)=>{
+            if(err){
+              res.status(404).send({
+                success: false,
+                message:'Error: Server error'});
+            }
+            else {
+
+              projectreviewer = prreviewer[0].reviewer;
+              var x=0;
+              for(i=0;i<contributors.length;i++)
+              {
+                Contributor.find({_id:contributors[i]},(err,c)=>{
+                  if(err){
+                    res.status(404).send({
+                      success: false,
+                      message:'Error: Server error'});
+                  }
+                  else {
+                    x=x+1;
+                    contributornames.push(c[0].name);
+                    //console.log(x);
+                    if(x===contributors.length)
+                    {
+                      res.status(200).send({
+                        success:true,
+                        message: 'Project found',
+                        description: project[0].description,
+                        pname:p[0].pname,
+                        reviewer:projectreviewer,
+                        contributors:contributornames
+                      });
+                    }
+                  }
+                });
+              }
+            }
+          });
+
+        }
+      });
+
+    }
+
+    else {
       Project.find({_id:pid},(err,p)=>{
         if(err){
           res.status(404).send({
@@ -48,7 +107,7 @@ function projectdesc(req,res,next){
                   else {
                     x=x+1;
                     contributornames.push(c[0].name);
-                    console.log(x);
+                    //console.log(x);
                     if(x===contributors.length)
                     {
                       res.status(200).send({
